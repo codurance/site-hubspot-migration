8b281ce773449fdf4430e7e7381ee836cd0b97e7
Daniel <info@daniel-bird.co.uk>
2020-11-30 17:01:20 +0000

    Hubspot changes (#29)

--- version_control/Codurance_September2020/modules/Training section.module/module.js
+++ version_control/Codurance_September2020/modules/Training section.module/module.js
@@ -2,13 +2,14 @@ $('.cm-training-card-group.mobile-slider').slick({
   dots: true,
   infinite: true,
   speed: 300,
-  adaptiveHeight:true,
+  adaptiveHeight:false,
   slidesToShow: 2,
   slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
-      settings: {
+      settings: {  
+        adaptiveHeight:false,
         slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
@@ -18,19 +19,29 @@ $('.cm-training-card-group.mobile-slider').slick({
     {
       breakpoint: 767,
       settings: {
+        adaptiveHeight:false,
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 2
       }
     },
     {
       breakpoint: 479,
       settings: {
+        adaptiveHeight:false,
         slidesToShow: 1,
         slidesToScroll: 1
       }
-    }
+    },
+     {
+      breakpoint: 320,
+      settings: {
+        adaptiveHeight:false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+     },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
   ]
-});
+  });();
