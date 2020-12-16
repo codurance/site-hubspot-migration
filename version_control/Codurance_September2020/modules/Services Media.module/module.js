35fa8f63f3f2aa026cd17441b69d53d257b64680
ewansheldon <ewansheldon@gmail.com>
2020-12-16 17:43:33 +0000

    adds buttons for filter

--- version_control/Codurance_September2020/modules/Services Media.module/module.js
+++ version_control/Codurance_September2020/modules/Services Media.module/module.js
@@ -2,7 +2,8 @@ const SELECTORS = {
     navigationControl: false,
     cardWindowSelector: '[data-services-media-card-window]',
     trackSelector: '[data-services-media-card-track]',
-    cardsSelector: '[data-services-media-card]'
+    cardsSelector: '[data-services-media-card]',
+    filters: ['podcast', 'video']
   };
   
   function init() {
