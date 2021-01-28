d815d18a7ae3d543b80a681d5b1bc6459f41a7f6
ewansheldon <ewansheldon@gmail.com>
2021-01-28 12:28:08 +0000

    reformat style of exit modal js

--- version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
+++ version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
@@ -1,16 +1,16 @@
-  let modalBackground = document.querySelector('[data-exit-popup-modal]');
+let modalBackground = document.querySelector('[data-exit-popup-modal]');
 
-  let closeButton = document.querySelector('[data-exit-popup-close-button]');
+let closeButton = document.querySelector('[data-exit-popup-close-button]');
 
-  closeButton.onclick = _ => {
+closeButton.onclick = _ => {
+  modalBackground.classList.add('hidden');
+}
+
+window.onclick = event => {
+  if (event.target == modalBackground) {
     modalBackground.classList.add('hidden');
   }
-
-  window.onclick = event => {
-    if (event.target == modalBackground) {
-      modalBackground.classList.add('hidden');
-    }
-  }
+}
 
 document.addEventListener("mouseout", evt => {
   if (evt.clientY < 10) {
