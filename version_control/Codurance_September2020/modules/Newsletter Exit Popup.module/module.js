292e63838406192843ec9691e8ae135b948c916a
ewansheldon <ewansheldon@gmail.com>
2021-02-02 16:30:02 +0000

    add back modal js

---
+++ version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
@@ -0,0 +1,20 @@
+let modalBackground = document.querySelector('[data-exit-popup-modal]');
+let closeButton = document.querySelector('[data-exit-popup-close-button]');
+let poppedUp = false;
+
+closeButton.addEventListener('click', _ => {
+  modalBackground.classList.add('hidden');
+});
+
+document.addEventListener('click', e => {
+  if (e.target == modalBackground) {
+    modalBackground.classList.add('hidden');
+  }
+});
+
+document.addEventListener("mouseout", e => {
+  if (e.clientY < 10 && !poppedUp) {
+    modalBackground.classList.remove('hidden');
+    poppedUp = true;
+  }
+});
