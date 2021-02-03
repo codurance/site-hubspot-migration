b56fd74908bda4c4a36d5986f30716dcfba25a68
ewansheldon <ewansheldon@gmail.com>
2021-02-03 17:20:20 +0000

    shows successful html on post

--- version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
+++ version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
@@ -1,20 +1,70 @@
 let modalBackground = document.querySelector('[data-exit-popup-modal]');
 let closeButton = document.querySelector('[data-exit-popup-close-button]');
-let poppedUp = false;
 
-closeButton.addEventListener('click', _ => {
+const closeModal = _ => {
   modalBackground.classList.add('hidden');
-});
+}
 
-document.addEventListener('click', e => {
+
+const checkClosingModal = e => {
   if (e.target == modalBackground) {
-    modalBackground.classList.add('hidden');
+    closeModal()
   }
-});
+}
 
-document.addEventListener("mouseout", e => {
-  if (e.clientY < 10 && !poppedUp) {
-    modalBackground.classList.remove('hidden');
-    poppedUp = true;
+
+const showModal = _ => {
+  modalBackground.classList.remove('hidden');
+  document.removeEventListener('mouseout', showModal);
+}
+
+const potentiallyLeavingPage = e => {
+  return e.clientY < 10;
+}
+
+const checkExit = e => {
+  if (potentiallyLeavingPage(e)) {
+    showModal();
   }
+}
+
+const addPopupEventListeners = _ => {
+  closeButton.addEventListener('click', closeModal);
+  document.addEventListener('click', checkClosingModal);
+  document.addEventListener('mouseout', checkExit);
+}
+
+const showSuccessfulFormHTML = _ => {
+  Array.prototype.slice.call(
+      document.querySelectorAll("[data-exit-popup-pending]")
+  ).forEach(element => {
+      element.classList.add('hidden');
+  });
+  
+  Array.prototype.slice.call(
+      document.querySelectorAll("[data-exit-popup-success]")
+  ).forEach(element => {
+      element.classList.remove('hidden');
+  })
+}
+
+const detectFormSubmission = _ => {
+  let form = document.querySelector(".newsletter-exit-popup__container form")
+
+  const observer = new MutationObserver(function(mutations_list) {
+      mutations_list.forEach(function(mutation) {
+          mutation.removedNodes.forEach(function(removed_node) {
+              if (removed_node === form) {
+                  showSuccessfulFormHTML()
+              }
+          });
+      });
+  });
+  
+  observer.observe(document.querySelector("#hs_form_target_newsletter_exit_popup_email_form"), { subtree: false, childList: true });
+}
+
+window.addEventListener('load', e => {
+  addPopupEventListeners();
+  detectFormSubmission();
 });
