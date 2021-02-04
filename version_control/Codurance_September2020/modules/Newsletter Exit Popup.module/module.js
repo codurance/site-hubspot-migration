cde009f919be5e4477feb82f8138fc8861d32471
ewansheldon <ewansheldon@gmail.com>
2021-02-04 10:14:40 +0000

    refactor form listening javascript

--- version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
+++ version_control/Codurance_September2020/modules/Newsletter Exit Popup.module/module.js
@@ -5,7 +5,6 @@ const closeModal = _ => {
   modalBackground.classList.add('hidden');
 }
 
-
 const checkClosingModal = e => {
   if (e.target == modalBackground) {
     closeModal();
@@ -47,22 +46,6 @@ const showSuccessfulFormHTML = _ => {
   })
 }
 
-const detectFormSubmission = _ => {
-  let form = document.querySelector(".newsletter-exit-popup__container form");
-
-  const observer = new MutationObserver(function(mutations_list) {
-      mutations_list.forEach(function(mutation) {
-          mutation.removedNodes.forEach(function(removed_node) {
-              if (removed_node === form) {
-                  showSuccessfulFormHTML();
-              }
-          });
-      });
-  });
-  
-  observer.observe(document.querySelector("#hs_form_target_newsletter_exit_popup_email_form"), { subtree: false, childList: true });
-}
-
 const disableExitPopup = _ => {
   document.removeEventListener('mouseout', checkExit);
 }
@@ -87,6 +70,5 @@ const listenForOtherFormSubmissions = _ => {
 
 window.addEventListener('load', e => {
   addPopupEventListeners();
-  detectFormSubmission();
   listenForOtherFormSubmissions();
 });
