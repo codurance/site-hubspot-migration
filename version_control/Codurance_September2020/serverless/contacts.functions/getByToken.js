83becff2fd3ee81a0a7432ed5471bf3bd0342921
ewansheldon <ewansheldon@gmail.com>
2021-02-24 17:31:42 +0000

    use async/await and iife function

--- version_control/Codurance_September2020/serverless/contacts.functions/getByToken.js
+++ version_control/Codurance_September2020/serverless/contacts.functions/getByToken.js
@@ -4,31 +4,23 @@ exports.main = (context, sendResponse) => {
     const { hapiKey } = process.env;
     const { hubspotutk } = context.params;
 
-    // const getContact = async _ => {
-    //     try {
+    const getContact = async _ => {
+        try {
             const url = `https://api.hubapi.com/contacts/v1/contact/utk/${hubspotutk}/profile?hapikey=${hapiKey}`
-    //         const response = await axios.get(url);
-    //         return response.data;
-    //     } catch (error) {
-    //         return error;
-    //     }
-    // }
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            return error;
+        }
+    }
 
-    // const isContact = async _ => {
-    //     const contact = await getContact();
-    //     return contact && !!contact.vid;
-    // }
+    const isContact = async _ => {
+        const contact = await getContact();
+        return contact && !!contact.vid;
+    }
 
-    // const res = { is_contact: isContact() };
-    // sendResponse({body: res, statusCode: 200});
-
-    axios.get(url)
-        .then(function (response) {
-            const contact = response.data
-            const isContact = contact && !!contact.vid
-            sendResponse({body: {is_contact: isContact}, statusCode: 200});
-        })
-        .catch(function (error) {
-            sendResponse({body: {is_contact: false}, statusCode: 422});
-        })
+    (async _ => {
+        const res = { is_contact: await isContact() };
+        sendResponse({body: res, statusCode: 200});
+    })()
  };
