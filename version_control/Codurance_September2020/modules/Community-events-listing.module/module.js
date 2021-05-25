3dfcc440769e6f51730e0df1cceb9672f2f13c3d
Stefi <stefania.rosca2309@gmail.com>
2021-05-25 15:15:16 +0200

    Refactoring getLocalTimeDate function

--- version_control/Codurance_September2020/modules/Community-events-listing.module/module.js
+++ version_control/Codurance_September2020/modules/Community-events-listing.module/module.js
@@ -11,21 +11,25 @@ const SELECTORS = {
 function init() {
   const cardSlider = new CardSlider(SELECTORS);
   cardSlider.init();
+  getLocalTimeDate();
+}
+
+function getLocalTimeDate() {
+let dateArray = document.querySelectorAll('.commmuity-events')
+
+  Array.from(dateArray).map(item => {
+      let timeToConvers = item.getAttribute('data-time')
+
+      const timeOptions = { hour12: false, hour: '2-digit', minute:'2-digit', timeZoneName: 'long'};
+      let localTime = new Date(parseInt(timeToConvers)).toLocaleTimeString("en-US", timeOptions)
+      
+      const dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+      let localDate = new Date(parseInt(timeToConvers)).toLocaleDateString("en-US", dateOptions )
+      localDate = localDate.replace(/,/g, ' ');
+
+      item.innerHTML = `${localDate} | ${localTime} `;
+  })
+
 }
 
 window.addEventListener('DOMContentLoaded', init);
-
-let dateArray = document.querySelectorAll('.commmuity-events')
-
-Array.from(dateArray).map(item => {
-  let timeToConvers = item.getAttribute('data-time')
-
-  const timeOptions = { hour12: false, hour: '2-digit', minute:'2-digit', timeZoneName: 'long'};
-  let localTime = new Date(parseInt(timeToConvers)).toLocaleTimeString("en-US", timeOptions)
-  
-  const dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-  let localDate = new Date(parseInt(timeToConvers)).toLocaleDateString("en-US", dateOptions )
-  localDate = localDate.replace(/,/g, ' ');
-
-  item.innerHTML = `${localDate} | ${localTime} `;
-})
