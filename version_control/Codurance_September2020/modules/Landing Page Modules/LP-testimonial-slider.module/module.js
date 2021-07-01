4d634d22f9cab5687522bcf8e2a3bf87fbef841b
Rodrigo Castro <rodrigocsastre@gmail.com>
2021-07-01 19:03:58 +0200

    Add the Object slider

---
+++ version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
@@ -0,0 +1,11 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
