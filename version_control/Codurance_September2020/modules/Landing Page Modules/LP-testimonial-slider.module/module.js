9e9f37a6cf79f7799015b12cf619c24af24f36a7
Rodrigo <rodrigocsastre@gmail.com>
2021-08-18 15:21:26 +0200

    New homepage (#165)
    * Set up page and add first section
    
    * Set a fallback colour with transparency
    
    * final master merge
    
    * Change background transparency
    
    * Define and set the homepage hero
    
    * Define the rules for the homepage hero container
    
    * Set rules for homepage-hero breakpoints
    
    * Created the Clients logos module
    
    * Adding the client logos module to the homepage
    
    * Change cta field clients name
    
    * Add padding and comments for sections
    
    * Add css rules and markup for client logos section
    
    * Create markup for all sections on homepage
    
    * Add img bg to consultancy section
    
    * Created module with text and image fields
    
    * Create css file and styles for text-image module
    
    * Include soft consultancy module on homepage
    
    * rename clients logos css file
    
    * Create default copy soft consultancy section
    
    * Create rules for soft consultancy section
    
    * Add renamed css file to client logos module
    
    * Adjust and create css rules for flex and bg
    
    * Create Compass module
    
    * Add Compass module to homepage
    
    * Fix font weight
    
    * Modify compass module markup
    
    * Add css file rename class selectors
    
    * Create css rules for compass module
    
    * Add and create partners section
    
    * Fine tune styles on compass module
    
    * Add shadow to the menu global component
    
    * Position Ethos module image
    
    * Delete default hs img attributes
    
    * Add and style contact us module
    
    * Add and create latest posts V2
    
    * Position and style recent posts cards
    
    * Create Community section & add it to homepage
    
    * Style community section on XL screens
    
    * Align posts and community cards on XL screens
    
    * Reduce min-height on header XL screens
    
    * Remove max-width on announcement-bar
    
    * Change font size on clients logos
    
    * Set a width on the compass image
    
    * Adjust padding on Clients logo section
    
    * Fix typo on property
    
    * Set newsletter sign up elements
    
    * Style newsletter section
    
    * Fix spacing on client logos and bold title
    
    * Icon alignment on compass and ethos section
    
    * Add style and layout to testimonial section
    
    * Hide scrollbar community cards
    
    * Fix grid on large screens on testimonials
    
    * Fix scroll on cards
    
    * Adapt and create custom background for testimonial
    
    * ALignment fixes on community and hero section
    
    * Change display page in the theme
    
    * Set a global content max width
    
    * Fix typo on class
    
    * Remove the max width on the homepage css
    
    * Fix typo
    
    * Arrange media queries with its class
    
    * Refactor using our own breakpoints

--- version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
+++ version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
@@ -4,6 +4,7 @@ var swiper = new Swiper(".swiper-container", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
