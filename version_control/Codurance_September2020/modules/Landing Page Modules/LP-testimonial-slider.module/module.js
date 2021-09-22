f07d6988f98b620b6c34028f9997242bbee18e78
Rodrigo <rodrigocsastre@gmail.com>
2021-09-22 12:52:51 +0200

    Hotfix/new-careers-page (#169)
    * Added arrows to the slider
    
    * Change font weight on Benefits section
    
    * Change hero spacing and font weight

--- version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
+++ version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
@@ -5,8 +5,15 @@ var swiper = new Swiper(".swiper", {
     spaceBetween: 30,
     loop: true,
     grabCursor: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    }
+    },
+    keyboard: {
+      enabled: true,
+    },
   });
