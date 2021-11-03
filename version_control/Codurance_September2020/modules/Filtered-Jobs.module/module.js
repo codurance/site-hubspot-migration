d48aa2f162a29beff99117b85be249c463d43c2d
Rodrigo Castro <rodrigocsastre@gmail.com>
2021-11-03 18:32:02 +0100

    Map and display a raw list from the items

--- version_control/Codurance_September2020/modules/Filtered-Jobs.module/module.js
+++ version_control/Codurance_September2020/modules/Filtered-Jobs.module/module.js
@@ -1,3 +1,24 @@
+const jobsItemsList = document.querySelector('#jobs-list');
+let jobList = [];
+
+
+const displayJobs = (jobs) => {
+  const htmlString = jobs.map(job => {
+    return `
+      <li class=job-item>
+        <p class="job-item__department">${job.department}</p>
+        <h3 class="job-item__title">${job.title}</h3>
+        <p class="job-item__telecommuting">${job.telecommuting ? "Remote" : "Hybrid"}</p>
+        <p class="job-item__city">${job.city}</p>
+        <p class="job-item__country">${job.country}</p>
+        <a class="ob-item__department text-cta-primary text-cta--right-arrow" href=${job.url}>Apply</a>
+      </li>
+    `
+  }).join('');
+
+  jobsItemsList.innerHTML = htmlString;
+
+}
 const getJobs = async () => {
   try{
       const urlEndpoint = "/_hcms/api/get_all_jobs";
