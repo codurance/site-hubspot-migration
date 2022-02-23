2a9b7159883edace424c625ebe758f7c26d6bfb2
Víctor Carmona <vicarali@gmail.com>
2022-02-23 12:13:22 +0100

    Refactor new features to avoid JS on HTML file

--- version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
+++ version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
@@ -1,25 +1,27 @@
-/* The swiper slider of the module will only be created
-if there is more than one slide */
-if(typeof swiperOneSlide == "undefined"){
-  let swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    watchOverflow: true,
-    grabCursor: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    keyboard: {
-      enabled: true,
-    },
-  });
-}
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  watchOverflow: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
 
+/* Check the number of slides on the swiper slider and
+disable it if there's just a single one (in that case,
+the length would be 3 due to the loop property) */
+if(swiper.slides.length == 3) {
+  swiper.destroy();
+}
 
 
