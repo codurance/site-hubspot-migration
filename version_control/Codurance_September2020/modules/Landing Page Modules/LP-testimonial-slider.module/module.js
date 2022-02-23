87493c71e5bc59699923a5bfb2051d4fdf54b445
Víctor Carmona <44123032+vicarali@users.noreply.github.com>
2022-02-23 16:10:55 +0100

    Adjustments to LP Testimonial Slider Module (#188)
    * Adjustments to LP Testimonial Slider Module
    
    * Refactor new features to avoid JS on HTML file
    
    * Minimal code refactoring to improve readability

--- version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
+++ version_control/Codurance_September2020/modules/Landing Page Modules/LP-testimonial-slider.module/module.js
@@ -1,19 +1,26 @@
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  watchOverflow: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
+
+const minimumSlidesAvailable = 3;
+
+if(swiper.slides.length == minimumSlidesAvailable) {
+  swiper.destroy();
+}
 
 
-var swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    keyboard: {
-      enabled: true,
-    },
-  });
