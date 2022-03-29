f2659de6964454be6b30e377603f82323f2fa395
Rodrigo Castro <rodrigocsastre@gmail.com>
2022-03-29 16:15:22 +0200

    Refactor anchor links spacing on scroll

--- version_control/Codurance_September2020/modules/secondary-menu.module/module.js
+++ version_control/Codurance_September2020/modules/secondary-menu.module/module.js
@@ -14,24 +14,11 @@ $(window).scroll(function(event) {
     previousViewpoint = currentViewpoint;
 });
 
-let anchorLinks = document.querySelectorAll('a[data-hs-anchor="true"]');
-let rootElement = document.documentElement;
-let menuHeight = getComputedStyle(rootElement).getPropertyValue("--menu-height");
 
 function fixMenu($menu) {
     $menu.addClass('secondary-menu--fixed');
-
-    anchorLinks.forEach(element => {
-        element.style.setProperty("--menu-height", menuHeight);
-    });
 }
 
 function dropMenu($menu) {
-    let menuHeightFormatted = parseFloat(menuHeight) * 2 + "rem";
-    
-    anchorLinks.forEach(element => {
-        element.style.setProperty("--menu-height", menuHeightFormatted);
-    });
-
     $menu.removeClass('secondary-menu--fixed');
 }
