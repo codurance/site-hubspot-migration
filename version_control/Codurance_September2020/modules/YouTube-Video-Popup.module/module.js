e5ae9fe46b99a5471803f58fc16e39cfba6f8a5c
Rodrigo Castro <rodrigocsastre@gmail.com>
2022-04-12 18:51:54 +0200

    Add basic functionality to the modal

--- version_control/Codurance_September2020/modules/YouTube-Video-Popup.module/module.js
+++ version_control/Codurance_September2020/modules/YouTube-Video-Popup.module/module.js
@@ -1,16 +1,51 @@
-const ungatedLinks = document.querySelectorAll(".icon-ungated");
 
-ungatedLinks.forEach(openVideoPopupOnClick);
+const ungatedButtons = document.querySelectorAll(".icon-ungated");
+const overlay = document.querySelector(".overlay");
+const popupVideo = document.querySelector(".modal__video");
+const closeModalButton = document.querySelector(".close-button");
 
-function openVideoPopupOnClick(ungatedLink){
-    ungatedLink.addEventListener("click", setVideoOnPopup);
+
+ungatedButtons.forEach(openVideoPopupOnClick);
+
+closeModalButton.addEventListener("click", function(){ hideModal(overlay) });
+
+function openVideoPopupOnClick(ungatedButton){
+    ungatedButton.addEventListener("click", setVideoOnPopup);
+}
+
+function showModal(elem) {
+    return elem.style.display = 'block';
+}
+
+function hideModal(elem) {
+    return elem.style.display = 'none';
+}
+
+
+function stripYoutubeID(str){
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;    
+    const match = str.match(regExp);
+    const idLength = 11;
+
+    if (match && match[2].length == idLength) return match[2];
+    
+    throw new Error('Expression not found');  
 }
 
 function setVideoOnPopup(event) {
-    const ungatedLinkHref = event.target.href;
-    let popupVideo = document.querySelector(".popup__video");
-
     event.preventDefault();
-    popupVideo.src = ungatedLinkHref;
+
+    const youtubeId = stripYoutubeID(event.target.href);
+    const embedPreferences = '?autoplay=1';
+    popupVideo.src = `https://www.youtube.com/embed/${youtubeId}${embedPreferences}`;
+    
+    showModal(overlay);
+
 }
+
+
+
+
+
+
 
