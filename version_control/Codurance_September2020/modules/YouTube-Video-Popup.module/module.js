dbe34660fe49b03ddd648ebdc320bd4bdd28cf35
Rodrigo <rodrigocsastre@gmail.com>
2022-04-19 12:06:04 -0300

    Feature/youtube-events-popup (#205)
    * Add button functionality for gated and ungated content
    
    * Create an overlay for gated content
    
    * Add dynamic icon buttons depending on button content
    
    * Change font size to fit button text
    
    * Style recent events section
    
    * Creation of new module to allocate the video popup
    
    * Set up initial JS functions of popup module
    
    * Style: add rough style to modal
    
    * refactor: unused elements and spaces
    
    * Refactor: module usability and button preferences
    
    * Refactor: naming some classes to be more explicit
    
    * Add basic functionality to the modal
    
    * Add: mobile breakpoints to edit on CMS editor
    
    * Include modal module for videos
    
    * Style: modal position and button
    
    * Add functionality to the modal and optional ways of scaping it
    
    * Update button copy and add animation to modal
    
    * Animation: update to be seen in different browsers
    
    * Update: button copy
    
    * Refactor: function parameters and DOM element values
    
    * Style: add cursor pointer on hover
    
    Co-authored-by: Víctor Carmona <vicarali@gmail.com>
    Co-authored-by: Rodrigo <RodrigoCastroS@users.noreply.github.com>

---
+++ version_control/Codurance_September2020/modules/YouTube-Video-Popup.module/module.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const ungatedButtons = document.querySelectorAll(".icon-ungated");
+const overlay = document.querySelector(".overlay");
+const popupVideo = document.querySelector(".modal__video");
+const closeModalButton = document.querySelector(".close-button");
+const modal = document.querySelector(".modal");
+
+ungatedButtons.forEach(openVideoPopupOnClick);
+
+closeModalButton.addEventListener("click", () => hideModal() );
+
+document.addEventListener('click', function(event){
+    if(event.target.className === "overlay") hideModal();
+})
+
+document.addEventListener('keyup', function(event){
+    if(event.code === "Escape") hideModal();
+})
+
+function openVideoPopupOnClick(ungatedButton){
+    ungatedButton.addEventListener("click", setVideoOnPopup);
+}
+
+function setVideoOnPopup(event) {
+    event.preventDefault();
+    
+    const youtubeId = stripYoutubeID(event.target.href);
+    const embedPreferences = '?autoplay=1';
+    popupVideo.src = `https://www.youtube.com/embed/${youtubeId}${embedPreferences}`;
+    
+    showModal();
+    
+}
+
+function stripYoutubeID(str){
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;    
+    const match = str.match(regExp);
+    const idLength = 11;
+
+    if (match && match[2].length == idLength) return match[2];
+    
+    throw new Error('Expression not found');  
+}
+
+function showModal() {
+    document.body.classList.add('overflow-hidden');
+    overlay.style.display = 'flex';
+    modal.style.opacity = '1';
+}
+
+function hideModal() {
+    document.body.classList.remove('overflow-hidden');
+    overlay.style.display = 'none';
+    modal.style.opacity = '0';
+    popupVideo.src = "";
+}
+
+
+
+
