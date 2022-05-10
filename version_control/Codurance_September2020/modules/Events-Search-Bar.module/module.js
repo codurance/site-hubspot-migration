a12fcb25cb00f784753273248811dd6e5830cf79
Víctor Carmona <44123032+vicarali@users.noreply.github.com>
2022-05-10 10:41:32 +0200

    Feature/past events search filter (#213)
    * Setup past events module dependencies
    
    * Add class parameter to card_collection macro
    
    Co-authored-by: Rodrigo <RodrigoCastroS@users.noreply.github.com>
    
    * Delete inclusion of Isotope JS library
    
    * Create search bar module with filtering system
    
    * Clean unused CSS code (remains from the JS slider)
    
    * Refactoring and improvement of animation functions
    
    * Adjustments to card collection transitions
    
    * Search bar initial CSS styling
    
    * Adjustments to improve filtering code readability
    
    Co-authored-by: Rodrigo <RodrigoCastroS@users.noreply.github.com>
    
    * Addition of icon on searchbar and accessibility
    
    * Minor adjustment for accessibility
    
    * Minor accessibility adjustment
    
    * Refactoring and addition of search results title
    
    * Search bar style adjustments
    
    * Refactor search icon and addition of reset button
    
    * Refactoring of header
    
    - Use standalone image selectable in CMS editor instead of background
    
    - New CSS styling and adjustments to previous one
    
    * Styling and minor refactoring of CSS animations
    
    * Creation of no results message field for module
    
    * Refactor and add message when there are no results
    
    * Simplify CSS class modifiers and overall animations
    
    Co-authored-by: Rodrigo <RodrigoCastroS@users.noreply.github.com>
    
    Co-authored-by: Rodrigo <RodrigoCastroS@users.noreply.github.com>

---
+++ version_control/Codurance_September2020/modules/Events-Search-Bar.module/module.js
@@ -0,0 +1,189 @@
+"use strict";
+
+const searchBarForm = document.querySelector(".events-search-bar");
+const searchBar = searchBarForm.querySelector(".events-search-bar__input");
+const searchBarResetButton = searchBarForm.querySelector(".events-search-bar__reset-button");
+
+searchBar.addEventListener("input", filterEventsOnInputValueChange);
+searchBarResetButton.addEventListener("click", filterEventsOnResetButtonClick);
+
+function filterEventsOnInputValueChange(inputEvent) {
+    const searchBarText = inputEvent.target.value;
+    
+    togglePromotedEvents(searchBarText);
+    toggleSearchResultsTitle(searchBarText);       
+    toggleSearchBarResetButton(searchBarText);
+    applyFilters(searchBarText);
+
+    // Timeout to adjust to the event card items animations
+    setTimeout(
+        toggleNoSearchResultsMessage,
+        getFadingAnimationDuration()
+    );
+}
+
+function filterEventsOnResetButtonClick() {
+    togglePromotedEvents();
+    toggleSearchResultsTitle();    
+    hideSearchBarResetButton();
+    applyFilters();
+}
+
+function togglePromotedEvents(searchBarText) {
+    const promotedEventsCollection = document.querySelector(".promoted-events");
+
+    if(searchBarText != "" && searchBarText != undefined) {
+        hideWithAnimation(promotedEventsCollection);
+    }
+    else {
+        showWithAnimation(promotedEventsCollection);
+    }           
+}
+
+function toggleSearchResultsTitle(searchBarText) {
+    const generalTitle = document.querySelector(".past-events .card-collection__title");
+    const searchResultsTitle = document.querySelector(".past-events .card-collection__search-results-title");
+
+    if (searchBarText != "" && searchBarText != undefined) {
+        showSearchResultsTitle(generalTitle, searchResultsTitle);
+    }
+    else {
+        setTimeout(
+            hideSearchResultsTitle, 
+            getFadingAnimationDuration(), 
+            generalTitle, searchResultsTitle
+        );
+    }
+}
+
+function showSearchResultsTitle(generalTitle, searchResultsTitle) {
+    addHiddenModifier(generalTitle);
+    removeHiddenModifier(searchResultsTitle);
+}
+
+function hideSearchResultsTitle(generalTitle, searchResultsTitle) {
+    removeHiddenModifier(generalTitle);
+    addHiddenModifier(searchResultsTitle);
+}
+
+function toggleSearchBarResetButton(searchBarText) {
+    if (searchBarText != "") {
+        showSearchBarResetButton();
+    }
+    else {
+        hideSearchBarResetButton();
+    }
+}
+
+function showSearchBarResetButton() {
+    searchBarForm.classList.add("events-search-bar--icon-hidden");
+    removeHiddenModifier(searchBarResetButton);
+}
+
+function hideSearchBarResetButton() {
+    searchBarForm.classList.remove("events-search-bar--icon-hidden");
+    addHiddenModifier(searchBarResetButton);
+}
+
+function applyFilters(searchBarText) {
+    if(arguments.length == 0) {
+        searchBarText = "";
+    }
+
+    const pastEventsCollection = document.querySelector(".past-events .card-collection-results");
+    const pastEvents = pastEventsCollection.querySelectorAll(".card-item");
+
+    pastEvents.forEach(event => filterEvent(event, searchBarText));
+}
+
+function filterEvent(event, searchBarText) {
+    if(searchTextInEvent(event, searchBarText)) {
+        showWithAnimation(event);            
+    }
+    else {
+        hideWithAnimation(event);
+    }
+}
+
+function searchTextInEvent(event, searchBarText) {
+    const eventItemTitle = event.querySelector(".card-item__title").innerText;
+    const eventItemDescription = event.querySelector(".card-item__description").innerText;
+    const regExp = createRegExpObject(searchBarText);
+
+    if(eventItemTitle.match(regExp) || 
+       eventItemDescription.match(regExp)
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
+function createRegExpObject(text) {
+    const regexpFlags = "i"; // Case Insensitive Flag
+    return new RegExp(text, regexpFlags);
+}
+
+function toggleNoSearchResultsMessage() {
+    const noSearchResultsMessage = document.querySelector(".card-collection-results__no-results-message");
+
+    if(areSearchResultsDisplayed()) {
+        hideWithAnimation(noSearchResultsMessage);
+    }
+    else {
+        showWithAnimation(noSearchResultsMessage);
+    }
+}
+
+function areSearchResultsDisplayed() {
+    const pastEventsCollection = document.querySelector(".past-events .card-collection-results");
+    const pastEvents = pastEventsCollection.querySelectorAll(".card-item");
+    
+    let searchResultsDisplayed = false;
+
+    pastEvents.forEach((event) => {
+        if(!event.classList.contains("hidden")) {
+            searchResultsDisplayed = true;
+        }
+    });
+
+    return searchResultsDisplayed;
+}
+
+function showWithAnimation(element) {
+    removeHiddenModifier(element);
+
+    // Timeout to show the transition before the display property changes
+    setTimeout(removeFadeAnimationModifier, getFadingAnimationDuration(), element); 
+}
+
+function hideWithAnimation(element) {
+    addFadeAnimationModifier(element);
+        
+    // Timeout to show the transition before the display property changes
+    setTimeout(addHiddenModifier, getFadingAnimationDuration(), element); 
+}
+
+function removeFadeAnimationModifier(element) {
+    element.classList.remove("fade-animation");
+}
+
+function addFadeAnimationModifier(element) {
+    element.classList.add("fade-animation");
+}
+
+function removeHiddenModifier(element) {
+    element.classList.remove("hidden");
+}
+
+function addHiddenModifier(element) {
+    element.classList.add("hidden");
+}
+
+function getFadingAnimationDuration() {
+    const rootHtml = document.documentElement;
+    const fadingAnimationDurationInMs = 
+        getComputedStyle(rootHtml).getPropertyValue("--fading-animation-duration");
+    
+    return parseInt(fadingAnimationDurationInMs);
+}
