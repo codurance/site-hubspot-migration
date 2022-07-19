1f9d1a34ff253c796a7ea157bb87263cd96fcd49
Víctor Carmona <vicarali@gmail.com>
2022-07-19 13:25:42 +0200

    Add blur effect feature on the read more blocks

--- version_control/Codurance_September2020/modules/Service-Lines-Listing.module/module.js
+++ version_control/Codurance_September2020/modules/Service-Lines-Listing.module/module.js
@@ -1,15 +1,39 @@
 "use strict";
 
-const readMoreCloseButton = 
-    document.querySelectorAll(".service-line__read-more-close-button");
+const readMoreBlocks = document.querySelectorAll(".service-line__read-more");
 
-readMoreCloseButton.forEach(button => collapseReadMoreOnClick(button));
+readMoreBlocks.forEach(
+    readMoreBlock => toggleReadMoreOnButtonClick(readMoreBlock)
+);
 
-function collapseReadMoreOnClick(readMoreCloseButton) {
+function toggleReadMoreOnButtonClick(readMoreBlock) {
+    const readMoreOpenButton = 
+        readMoreBlock.querySelector(".service-line__read-more-title");
+    const readMoreCloseButton = 
+        readMoreBlock.querySelector(".service-line__read-more-close-button");
+
+    readMoreOpenButton.addEventListener(
+        "click", 
+        () => { toggleReadMoreOverflow(readMoreBlock) }
+    );
+    readMoreCloseButton.addEventListener(
+        "click", 
+        () => { toggleReadMoreOverflow(readMoreBlock) }
+    );
+
     readMoreCloseButton.addEventListener("click", collapseReadMore);
 }
 
 function collapseReadMore(event) {
     const readMoreCloseButton = event.target;
+
     readMoreCloseButton.parentNode.removeAttribute("open");
+}
+
+function toggleReadMoreOverflow(readMoreBlock) {
+    const serviceLineDescription = readMoreBlock.previousElementSibling;
+
+    serviceLineDescription.classList.toggle(
+        "service-line__description--read-more"
+    );
 }
