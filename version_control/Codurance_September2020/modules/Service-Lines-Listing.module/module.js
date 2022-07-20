0929231f7ff551d0af499ad8033616757d74051d
Víctor Carmona <44123032+vicarali@users.noreply.github.com>
2022-07-20 17:40:53 +0200

    Feature/case-study-template/service-lines (#242)
    * Creation of module
    
    * Rename text field
    
    * Initial setup of the layout of the module
    
    * Add decorative line to titles and minor styling
    
    * Change gap between icon and text
    
    * Add "Read more" feature for extra text content
    
    * Add blur effect feature on the read more blocks
    
    * Set small padding on top of the listing texts
    
    * Add help text to description field of module
    
    * Add fixed aspect-ratio to icon images of module
    
    * Create dnd section of the new module
    
    * Change default sizing of new header img to auto
    
    * Change approach of CSS of module and corrections
    
    The layout was refactored following a mobile-first approach
    
    * Create group for "read more" fields of module
    
    * Add aria-label to the close button of Read More
    
    * Rename class and minor refactoring of JS code

---
+++ version_control/Codurance_September2020/modules/Service-Lines-Listing.module/module.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const readMoreBlocks = document.querySelectorAll(".service-line__read-more");
+
+readMoreBlocks.forEach(
+    readMoreBlock => toggleReadMoreButton(readMoreBlock)
+);
+
+function toggleReadMoreButton(readMoreBlock) {
+    const readMoreOpenButton = 
+        readMoreBlock.querySelector(".service-line__read-more-open-button");
+    const readMoreCloseButton = 
+        readMoreBlock.querySelector(".service-line__read-more-close-button");
+
+    readMoreOpenButton.addEventListener(
+        "click", 
+        () => { toggleReadMoreOverflow(readMoreBlock) }
+    );
+    readMoreCloseButton.addEventListener(
+        "click", 
+        (event) => { 
+            toggleReadMoreOverflow(readMoreBlock);
+            collapseReadMore(event);
+        }
+    );
+}
+
+function collapseReadMore(event) {
+    const readMoreCloseButton = event.target;
+
+    readMoreCloseButton.parentNode.removeAttribute("open");
+}
+
+function toggleReadMoreOverflow(readMoreBlock) {
+    const serviceLineDescription = readMoreBlock.previousElementSibling;
+
+    serviceLineDescription.classList.toggle(
+        "service-line__description--read-more"
+    );
+}
