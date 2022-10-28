e3abee5431d60c0654e49d7973aec2b86ee49aee
Rodrigo <rodrigocsastre@gmail.com>
2022-10-28 10:07:29 +0200

    Feature/blog-search (#278)
    * Add search input and page results from hubspot modules
    
    * Add basic style to search input and search suggestions
    
    * Minor styling improvements
    
    * Extract CSS from search module to final location
    
    * Rename masonry library file
    
    * Update style on input and suggested results
    
    * Fix background issue on the search bar
    
    * Add option to clear results when clicking outside of them
    
    * Adjust input position and background
    
    * Create design for the search results page
    
    * Change colors from links from search bar dropdown
    
    * Add outline on the search bar when focused
    
    * Add go back button to search results list
    
    * Change default search results module for custom
    
    * Extract CSS from search results listing
    
    * Style previous/next navigation links
    
    * Configure arrow links macros and refactor links
    
    * Fix next page link showing left on first page
    
    * Add ability to translate the texts from results
    
    * Change translations for dynamic buttons
    
    * Style seach button
    
    * Refactor code and translate empty results
    
    * Adapt padding for blog listing displayed
    
    * Style adjustments for search button
    
    * Implement language search parameter
    
    * Include search bar on spanish listing page
    
    * Change var declarations to let
    
    * Fix erroneous translations on search results page
    
    * Set max num of lines on search results excerpts
    
    * Minor blog header height adjustments
    
    * Create gutter for cards
    
    * Style, size cards and delete dead css
    
    * Align intro text on ES blog posts
    
    * Refactor search button on mobile screeens
    
    * Add box-shadow to search bar button
    
    * Refactor box-shadow and outlines from search bar
    
    * Add double quote surrounding search terms
    
    * Change conditional on pagination language
    
    * Change url ES redirection on page results
    
    * Refactor retrieving the no results message
    
    * Adjust arrow CTA links
    
    * Adjust format from global search results styles
    
    * Change template type of search bar to blog listing
    
    * Adjust format of global search input JS
    
    * Remove template types (search results module)
    
    * Remove unused linked libraries on search results
    
    Co-authored-by: Víctor Carmona <vicarali@gmail.com>

---
+++ version_control/Codurance_September2020/modules/Global-Search-Results.module/module.js
@@ -0,0 +1,178 @@
+const noResults = {
+  en: {
+    title: "Sorry. There are no results for",
+    subtitle: "Try rewording your query, or browse through our site."
+  },
+  es: {
+    title: "Disculpa. No encontramos resultados para",
+    subtitle: "Intenta cambiar tu búsqueda, o navega a través de nuestra web"
+  }
+}
+
+let hsResultsPage = function(_resultsClass) {
+  function buildResultsPage(_instance) {
+    let resultTemplate = _instance.querySelector(
+        '.hs-search-results__template'
+      ),
+      resultsSection = _instance.querySelector('.hs-search-results__listing'),
+      searchPath = _instance
+        .querySelector('.hs-search-results__pagination')
+        .getAttribute('data-search-path'),
+      prevLink = _instance.querySelector('.hs-search-results__prev-page'),
+      nextLink = _instance.querySelector('.hs-search-results__next-page');
+
+    let searchParams = new URLSearchParams(window.location.search.slice(1));
+
+    function getTerm() {
+      return searchParams.get('term') || '';
+    }
+    function getOffset() {
+      return parseInt(searchParams.get('offset')) || 0;
+    }
+    function getLimit() {
+      return parseInt(searchParams.get('limit'));
+    }
+    function addResult(title, url, description, featuredImage) {
+      let newResult = document.importNode(resultTemplate.content, true);
+      function isFeaturedImageEnabled() {
+        if (
+          newResult.querySelector('.hs-search-results__featured-image > img')
+        ) {
+          return true;
+        }
+      }
+      newResult.querySelector('.hs-search-results__title').innerHTML = title;
+      newResult.querySelector('.hs-search-results__title').href = url;
+      newResult.querySelector(
+        '.hs-search-results__description'
+      ).innerHTML = description;
+      if (typeof featuredImage !== 'undefined' && isFeaturedImageEnabled()) {
+        newResult.querySelector(
+          '.hs-search-results__featured-image > img'
+        ).src = featuredImage;
+      }
+      resultsSection.appendChild(newResult);
+    }
+    function fillResults(results) {
+      results.results.forEach(function(result, i) {
+        addResult(
+          result.title,
+          result.url,
+          result.description,
+          result.featuredImageUrl
+        );
+      });
+    }
+
+    function emptyPagination() {
+      prevLink.innerHTML = '';
+      nextLink.innerHTML = '';
+    }
+
+    function emptyResults(searchedTerm) {
+      const content = searchParams.get('language') === 'es' ? noResults.es : noResults.en;
+
+
+      const noResultsHtml = `<div class="hs-search__no-results">
+                                    <p>${content.title} ${searchedTerm} </p>
+                                    <p>${content.subtitle}</p>
+                               </div>`;
+
+      resultsSection.insertAdjacentHTML('afterbegin', noResultsHtml);
+    }
+
+    function setSearchBarDefault(searchedTerm) {
+      let searchBars = document.querySelectorAll('.hs-search-field__input');
+      Array.prototype.forEach.call(searchBars, function(el) {
+        el.value = searchedTerm;
+      });
+    }
+
+    function httpRequest(term, offset) {
+      let SEARCH_URL = '/_hcms/search?',
+        requestUrl = SEARCH_URL + searchParams + '&analytics=true',
+        request = new XMLHttpRequest();
+
+      request.open('GET', requestUrl, true);
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          let data = JSON.parse(request.responseText);
+          setSearchBarDefault(data.searchTerm);
+          if (data.total > 0) {
+            fillResults(data);
+            paginate(data);
+          } else {
+            emptyResults(data.searchTerm);
+            emptyPagination();
+          }
+        } else {
+          console.error('Server reached, error retrieving results.');
+        }
+      };
+      request.onerror = function() {
+        console.error('Could not reach the server.');
+      };
+      request.send();
+    }
+
+    function paginate(results) {
+      let updatedLimit = getLimit() || results.limit;
+
+      function hasPreviousPage() {
+        return results.page > 0;
+      }
+      function hasNextPage() {
+        return results.offset <= results.total - updatedLimit;
+      }
+
+      if (hasPreviousPage()) {
+        let prevParams = new URLSearchParams(searchParams.toString());
+        prevParams.set(
+          'offset',
+          results.page * updatedLimit - parseInt(updatedLimit)
+        );
+        prevLink.href = '/' + searchPath + '?' + prevParams;
+      } else {
+        prevLink.parentNode.removeChild(prevLink);
+      }
+
+      if (hasNextPage()) {
+        let nextParams = new URLSearchParams(searchParams.toString());
+        nextParams.set(
+          'offset',
+          results.page * updatedLimit + parseInt(updatedLimit)
+        );
+        nextLink.href = '/' + searchPath + '?' + nextParams;
+      } else {
+        nextLink.parentNode.removeChild(nextLink);
+      }
+    }
+
+    let getResults = (function() {
+      if (getTerm()) {
+        httpRequest(getTerm(), getOffset());
+      } else {
+        emptyPagination();
+      }
+    })();
+  }
+
+  (function() {
+    let searchResults = document.querySelectorAll(_resultsClass);
+    Array.prototype.forEach.call(searchResults, function(el) {
+      buildResultsPage(el);
+    });
+  })();
+};
+
+if (
+  document.attachEvent
+    ? document.readyState === 'complete'
+    : document.readyState !== 'loading'
+) {
+  let resultsPages = hsResultsPage('div.hs-search-results');
+} else {
+  document.addEventListener('DOMContentLoaded', function() {
+    let resultsPages = hsResultsPage('div.hs-search-results');
+  });
+}
