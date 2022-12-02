c7b2a5d2a0bc47308877a14c3923089f9a1aabc1
Víctor Carmona Aliu <44123032+vicarali@users.noreply.github.com>
2022-12-02 17:29:37 +0100

    Update/our-roles/videos-listing-to-single-video (#291)
    * Add new video_urls field
    
    * Refactor clickable-listing to add dynamic video
    
    * Add effect to corresponding link of active video
    
    * Add wrapper to links listing to extract gradient
    
    * Add feature to change the video dynamically
    
    * Adjust lateral spacing on listing module
    
    * Fix further lateral-spacing issues
    
    * Design adjustments to improve UX on mobile
    
    * Padding adjustment on header
    
    Co-authored-by: Rodrigo <rodrigocsastre@gmail.com>

---
+++ version_control/Codurance_September2020/modules/Clickable-Listing.module/module.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let dynamicVideo;
+
+document.addEventListener("DOMContentLoaded", initDynamicVideo);
+
+function initDynamicVideo() {
+    dynamicVideo = document.querySelector(".dynamic-video");
+
+    if(dynamicVideo) initClickableLinks();
+}
+
+function initClickableLinks() {
+    const clickableLinks = document.querySelectorAll(".listing__item");
+
+    clickableLinks.forEach(clickableLink => 
+        clickableLink.addEventListener("click", changeDynamicVideoByLink)
+    );
+}
+
+function changeDynamicVideoByLink(event) {
+    const clickableLink = event.target.closest("a");
+
+    event.preventDefault();
+
+    dynamicVideo.src = clickableLink.href;
+    changeActiveClickableLink(clickableLink);
+}
+
+function changeActiveClickableLink(clickableLink) {
+    const activeLink = document.querySelector(".listing__item--active");
+
+    activeLink.classList.remove("listing__item--active");
+    clickableLink.classList.add("listing__item--active");
+}
