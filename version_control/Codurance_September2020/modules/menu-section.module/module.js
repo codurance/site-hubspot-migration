5ee920fbf4ad40b5992452b08d43577c49692b0c
Rodrigo C <rodrigocsastre@gmail.com>
2023-06-12 14:01:42 +0200

    Refine accesibility to open the menu with the keyboard

--- version_control/Codurance_September2020/modules/menu-section.module/module.js
+++ version_control/Codurance_September2020/modules/menu-section.module/module.js
@@ -1,33 +1,37 @@
-var menuLinks = document.querySelectorAll(".submenu > li .menu-link");
-var firstSubmenuItems = document.querySelectorAll(
+let menuLinks = document.querySelectorAll(".menu-link");
+let firstSubmenuItems = document.querySelectorAll(
   ".submenu.level-2 > *:nth-child(2)"
 );
 
-menuLinks.forEach(function(link) {
+menuLinks.forEach((link) => {
   link.addEventListener("focus", function() {
     link.parentElement.classList.add("focus");
   });
 
   if (link.nextElementSibling) {
-    var subLinks = document.querySelectorAll(".level-2 li > .menu-link");
-    var lastLinkIndex = subLinks.length - 1;
-    var lastLink = subLinks[lastLinkIndex];
+    console.log(link.nextElementSibling);
+    const submenu = link.nextElementSibling;
+    const subMenuLinks = submenu.querySelectorAll(".submenu-item");
+    const lastLinkIndex = subMenuLinks.length - 1;
+    const lastLink = subMenuLinks[lastLinkIndex];
+
     lastLink.addEventListener("blur", function() {
-      link.parentElement.classList.remove("focus");
+      lastLink.closest(".menu-item").classList.remove("focus");
+      // link.parentElement.classList.remove("focus");
     });
   }
 });
 
-firstSubmenuItems.forEach(function(item) {
-  if (window.hsInEditor) {
-    return;
-  } else if (item) {
-    item.addEventListener("mouseover", function() {
-      item.previousElementSibling.classList.add("hover");
-    });
+// firstSubmenuItems.forEach(function(item) {
+//   if (window.hsInEditor) {
+//     return;
+//   } else if (item) {
+//     item.addEventListener("mouseover", function() {
+//       item.previousElementSibling.classList.add("hover");
+//     });
 
-    item.addEventListener("mouseout", function() {
-      item.previousElementSibling.classList.remove("hover");
-    });
-  }
-});
+//     item.addEventListener("mouseout", function() {
+//       item.previousElementSibling.classList.remove("hover");
+//     });
+//   }
+// });
