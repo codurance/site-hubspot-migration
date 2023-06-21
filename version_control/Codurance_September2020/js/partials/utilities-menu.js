680cfb1b01c3e9096f04d69041b0fb208554b11d
Víctor Carmona <vicarali@gmail.com>
2023-06-21 18:43:53 +0200

    Control focus and blur events on search bar input

--- version_control/Codurance_September2020/js/partials/utilities-menu.js
+++ version_control/Codurance_September2020/js/partials/utilities-menu.js
@@ -4,11 +4,23 @@ const searchBarButton = document.querySelector(
   ".utilities-menu .search-bar__button"
 );
 const searchBar = document.querySelector(".utilities-menu .hs-search-field");
+const searchBarInput = document.querySelector(
+  ".utilities-menu .hs-search-field__input"
+);
 
 searchBarButton.addEventListener("mouseenter", () => {
   searchBarButton.setAttribute("aria-expanded", "true");
 });
 
+searchBarButton.addEventListener("focus", () => {
+  searchBarButton.setAttribute("aria-expanded", "true");
+});
+
 searchBar.addEventListener("mouseleave", () => {
+  if (searchBarInput === document.activeElement) return;
+  searchBarButton.setAttribute("aria-expanded", "false");
+});
+
+searchBarInput.addEventListener("blur", () => {
   searchBarButton.setAttribute("aria-expanded", "false");
 });
