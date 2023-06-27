b2492aca90dc51baebb5bc092e8a1b89756cb191
Víctor Carmona Aliu <44123032+vicarali@users.noreply.github.com>
2023-06-27 11:18:33 +0200

    Feature/main-navigation-menu/search-bar (#398)
    * Extract navigation link from utilities menu module
    
    * Add stylesheet on new navigation link module
    
    * Refactor utilities menu module to lang selector
    
    * Set up partial for the utilities menu modules
    
    * Adjust spacing between utilities
    
    * Remove background utility class from search bar
    
    * Add toggable search bar on hover
    
    * Control focus and blur events on search bar input
    
    * Fix blur event triggering on search suggestions
    
    * Display search bar from the right instead
    
    * Update hubspot search API
    
    * Only close search bar on blur if no suggestions
    
    * Typography styling
    
    * Style search bar spacing and button
    
    * Initial responsive adjustments
    
    * Set PROD portal ID on search bar
    
    * Make mobile menu trigger focusable
    
    * Position search bar on mobile
    
    * Hide search bar on submenu opened
    
    * Increase spacing on hamburguer menu on mobile
    
    * Change typography size and weight on mobile
    
    * Hide info results of author and date when it's a regular site page
    
    * Change opening/closing quotes
    
    * Set lang selector position to match baseline alignment with logo on mobile
    
    * Improve search bar animation sliding in and out
    
    * Delete unused variable
    
    * Display error message when search input is empty
    
    * Remove logs
    
    * Delete unused js file
    
    ---------
    
    Co-authored-by: Rodrigo C <rodrigocsastre@gmail.com>

--- version_control/Codurance_September2020/modules/Global-Search-Results.module/module.js
+++ version_control/Codurance_September2020/modules/Global-Search-Results.module/module.js
@@ -29,9 +29,11 @@ let hsResultsPage = function(_resultsClass) {
     function getOffset() {
       return parseInt(searchParams.get("offset")) || 0;
     }
+
     function getLimit() {
       return parseInt(searchParams.get("limit"));
     }
+
     function addResult(
       title,
       url,
@@ -41,6 +43,16 @@ let hsResultsPage = function(_resultsClass) {
       authorFullName
     ) {
       let newResult = document.importNode(resultTemplate.content, true);
+
+      const author = newResult.querySelector(".hs-search-results__author");
+      const image = newResult.querySelector(
+        ".hs-search-results__featured-image > img"
+      );
+      const titleTag = newResult.querySelector(".hs-search-results__title");
+      const descriptionTag = newResult.querySelector(
+        ".hs-search-results__description"
+      );
+
       function isFeaturedImageEnabled() {
         if (
           newResult.querySelector(".hs-search-results__featured-image > img")
@@ -48,27 +60,27 @@ let hsResultsPage = function(_resultsClass) {
           return true;
         }
       }
-      newResult.querySelector(".hs-search-results__title").innerHTML = title;
-      newResult.querySelector(".hs-search-results__title").href = url;
-      newResult.querySelector(
-        ".hs-search-results__description"
-      ).innerHTML = description;
 
-      newResult.querySelector(
-        ".hs-search-results__date"
-      ).textContent = getAndFormatDate(publishedDate);
+      titleTag.innerHTML = title;
+      titleTag.href = url;
+      descriptionTag.innerHTML = description;
 
-      newResult.querySelector(
-        ".hs-search-results__author"
-      ).textContent = authorFullName;
+      if (typeof publishedDate !== "undefined") {
+        newResult.querySelector(
+          ".hs-search-results__date"
+        ).textContent = `· Posted ${getAndFormatDate(publishedDate)}`;
+      }
+
+      if (typeof authorFullName !== "undefined") {
+        author.textContent = `By ${authorFullName}`;
+      }
 
       if (typeof featuredImage !== "undefined" && isFeaturedImageEnabled()) {
-        newResult.querySelector(
-          ".hs-search-results__featured-image > img"
-        ).src = featuredImage;
+        image.src = featuredImage;
       }
       resultsSection.appendChild(newResult);
     }
+
     function getAndFormatDate(publishedDate) {
       const dateOptions = {
         year: "numeric",
@@ -105,9 +117,12 @@ let hsResultsPage = function(_resultsClass) {
         searchParams.get("language") === "es" ? noResults.es : noResults.en;
 
       const noResultsHtml = `<div class="hs-search__no-results">
-                                    <p>${content.title} ${searchedTerm} </p>
+                                    <p>
+                                      ${content.title} 
+                                      “${searchedTerm ? searchedTerm : ""}”
+                                    </p>
                                     <p>${content.subtitle}</p>
-                               </div>`;
+                              </div>`;
 
       resultsSection.insertAdjacentHTML("afterbegin", noResultsHtml);
     }
@@ -183,6 +198,7 @@ let hsResultsPage = function(_resultsClass) {
       if (getTerm()) {
         httpRequest(getTerm(), getOffset());
       } else {
+        emptyResults(getTerm());
         emptyPagination();
       }
     })();
