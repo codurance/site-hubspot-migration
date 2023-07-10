3c8483fe1dcd895dcfbd4e67a22c9be398239be0
Rodrigo C <rodrigocsastre@gmail.com>
2023-07-10 17:02:14 +0200

    Display searchbar on hover and focus on input when clicked

--- version_control/Codurance_September2020/js/partials/utilities-menu.js
+++ version_control/Codurance_September2020/js/partials/utilities-menu.js
@@ -11,26 +11,30 @@ const searchSuggestions = document.querySelector(
   ".utilities-menu .hs-search-field__suggestions"
 );
 
-searchBarButton.addEventListener("mouseenter", () => {
+searchBarButton.addEventListener("click", () => {
   searchBarButton.setAttribute("aria-expanded", "true");
+  setTimeout(() => searchBarInput.focus(), 500);
 });
 
 searchBarButton.addEventListener("focus", () => {
   searchBarButton.setAttribute("aria-expanded", "true");
+  setTimeout(() => searchBarInput.focus(), 500);
 });
 
-searchBar.addEventListener("mouseleave", () => {
+searchBar.addEventListener("click", () => {
   if (searchBarInput === document.activeElement) return;
   searchBarButton.setAttribute("aria-expanded", "false");
-});
-
-searchBarButton.addEventListener("mouseenter", () => {
-  searchBarButton.setAttribute("aria-expanded", "true");
 });
 
 searchBarInput.addEventListener("blur", () => {
   if (searchBar.classList.contains("hs-search-field--open")) return;
   searchBarButton.setAttribute("aria-expanded", "false");
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.key === "Esc") {
+    searchBarButton.setAttribute("aria-expanded", "false");
+  }
 });
 
 // This is required to stop the blur event when clicking on suggestions
