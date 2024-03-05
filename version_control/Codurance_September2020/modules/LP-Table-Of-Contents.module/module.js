a55d6717f5fc9e7623b5006f9dd10f4055b800cf
VictorC <vicarali@gmail.com>
2024-03-05 14:55:46 +0100

    Refactor table of contents in order to only show submenu of active section

--- version_control/Codurance_September2020/modules/LP-Table-Of-Contents.module/module.js
+++ version_control/Codurance_September2020/modules/LP-Table-Of-Contents.module/module.js
@@ -5,3 +5,33 @@ const mostExternalContainer = tableOfContents.closest(".dnd-column > div");
 
 mostExternalContainer.classList.add("sticky-menu");
 
+const anchorLinks = document.querySelectorAll("a[data-hs-anchor]");
+const observer = new IntersectionObserver(handleObserve);
+anchorLinks.forEach((anchorLink) => {
+  const textSection = anchorLink.parentElement;
+  observer.observe(textSection);
+});
+
+function handleObserve(textSections) {
+  textSections.forEach(showSubmenu);
+}
+
+function showSubmenu(textSection) {
+  const anchorId = textSection.target.querySelector("a[data-hs-anchor]").id;
+  const tableLink = document.querySelector(
+    "a[role='menuitem'][href='#" + anchorId + "']"
+  );
+  let subMenu;
+
+  if (tableLink) {
+    subMenu = tableLink.parentElement.querySelector(
+      ".hs-menu-children-wrapper"
+    );
+
+    subMenu.classList.toggle("table-of-contents__menu--shown");
+  }
+
+  if (!textSection.isIntersecting)
+    subMenu.classList.remove("table-of-contents__menu--shown");
+}
+
