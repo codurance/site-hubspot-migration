2e8c400b00b8608716884672eae14ffed944b08f
VictorC <vicarali@gmail.com>
2024-03-06 18:02:47 +0100

    Refactor table of contents module in order to highlight level 1 link when section is active

--- version_control/Codurance_September2020/modules/LP-Table-Of-Contents.module/module.js
+++ version_control/Codurance_September2020/modules/LP-Table-Of-Contents.module/module.js
@@ -1,37 +1,39 @@
 "use strict";
 
-const tableOfContents = document.querySelector(".table-of-contents");
-const mostExternalContainer = tableOfContents.closest(".dnd-column > div");
+document.addEventListener("DOMContentLoaded", () => {
+  const tableOfContents = document.querySelector(".table-of-contents");
+  const mostExternalContainer = tableOfContents.closest(".dnd-column > div");
 
-mostExternalContainer.classList.add("sticky-menu");
+  mostExternalContainer.classList.add("sticky-menu");
 
-const anchorLinks = document.querySelectorAll("a[data-hs-anchor]");
-const observer = new IntersectionObserver(handleObserve);
-anchorLinks.forEach((anchorLink) => {
-  const textSection = anchorLink.parentElement;
-  observer.observe(textSection);
-});
+  const anchorLinks = document.querySelectorAll("a[data-hs-anchor]");
+  const observer = new IntersectionObserver(handleObserve);
+  anchorLinks.forEach((anchorLink) => {
+    const textSection = anchorLink.parentElement;
+    observer.observe(textSection);
+  });
 
-function handleObserve(textSections) {
-  textSections.forEach(showSubmenu);
-}
-
-function showSubmenu(textSection) {
-  const anchorId = textSection.target.querySelector("a[data-hs-anchor]").id;
-  const tableLink = document.querySelector(
-    "a[role='menuitem'][href='#" + anchorId + "']"
-  );
-  let subMenu;
-
-  if (tableLink) {
-    subMenu = tableLink.parentElement.querySelector(
-      ".hs-menu-children-wrapper"
-    );
-
-    subMenu.classList.toggle("table-of-contents__menu--shown");
+  function handleObserve(textSections) {
+    textSections.forEach(showSubmenu);
   }
 
-  if (!textSection.isIntersecting)
-    subMenu.classList.remove("table-of-contents__menu--shown");
-}
+  function showSubmenu(textSection) {
+    const anchorId = textSection.target.querySelector("a[data-hs-anchor]").id;
+    const tableLink = document.querySelector(
+      "a[role='menuitem'][href='#" + anchorId + "']"
+    );
+    let menu;
+
+    if (tableLink) {
+      menu = tableLink.parentElement;
+
+      menu.classList.toggle("table-of-contents__menu--shown");
+    }
+
+    if (tableLink && !textSection.isIntersecting) {
+      console.log("adios");
+      menu.classList.remove("table-of-contents__menu--shown");
+    }
+  }
+});
 
