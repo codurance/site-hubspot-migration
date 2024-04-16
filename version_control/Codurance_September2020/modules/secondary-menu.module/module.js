a8ad91d2518fc5505cdf9335eaceaf6b5358a6de
VictorC <vicarali@gmail.com>
2024-04-16 17:48:21 +0200

    Refactor secondary menu module to not use jQuery

--- version_control/Codurance_September2020/modules/secondary-menu.module/module.js
+++ version_control/Codurance_September2020/modules/secondary-menu.module/module.js
@@ -1,5 +1,6 @@
 let previousViewpoint = 0;
-const navigationHeight = document.querySelector(".secondary-menu").offsetHeight;
+const secondaryMenu = document.querySelector(".secondary-menu");
+const navigationHeight = secondaryMenu.offsetHeight;
 const mainMenuHeight = document.querySelector(".header").offsetHeight;
 
 document.documentElement.style.setProperty(
@@ -7,22 +8,22 @@ document.documentElement.style.setProperty(
   navigationHeight + mainMenuHeight + "px"
 );
 
-$(window).scroll(function (event) {
-  let currentViewpoint = $(this).scrollTop();
+window.addEventListener("scroll", () => {
+  let currentViewpoint = window.scrollY;
 
   if (currentViewpoint > previousViewpoint) {
-    fixMenu($(".secondary-menu"));
+    fixMenu(secondaryMenu);
   } else {
-    dropMenu($(".secondary-menu"));
+    dropMenu(secondaryMenu);
   }
 
   previousViewpoint = currentViewpoint;
 });
 
-function fixMenu($menu) {
-  $menu.addClass("secondary-menu--fixed");
+function fixMenu(menu) {
+  menu.classList.add("secondary-menu--fixed");
 }
 
-function dropMenu($menu) {
-  $menu.removeClass("secondary-menu--fixed");
+function dropMenu(menu) {
+  menu.classList.remove("secondary-menu--fixed");
 }
