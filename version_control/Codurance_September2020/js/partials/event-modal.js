af4a9b17f53d4b8d4c71db5ea308873da0741f6e
VictorC <vicarali@gmail.com>
2024-05-16 16:27:53 +0200

    Set up and style event title and initial state of load event info function

--- version_control/Codurance_September2020/js/partials/event-modal.js
+++ version_control/Codurance_September2020/js/partials/event-modal.js
@@ -7,6 +7,20 @@ const eventCardButtons = document.querySelectorAll(
 
 eventCardButtons.forEach((button) =>
   button.addEventListener("click", () => {
+    const eventCard = button.closest(".card-slider-item");
+    loadEventToModal(eventCard);
     eventModal.showModal();
   })
 );
+
+function loadEventToModal(eventCard) {
+  const eventTitle = eventCard.dataset.eventTitle;
+  const eventDescription = eventCard.dataset.eventDescription;
+  const eventLink = eventCard.dataset.eventLink;
+  const eventType = eventCard.dataset.eventType;
+  const eventDate = eventCard.dataset.eventDate;
+  const eventBackgroundImage = eventCard.dataset.eventBackgroundImage;
+
+  const modalTitle = eventModal.querySelector(".event-modal__title");
+  modalTitle.textContent = eventTitle;
+}
